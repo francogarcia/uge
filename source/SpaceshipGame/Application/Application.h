fa2e9dbf24766bcc2385c73255530c43f7352973
Franco Garcia <francoegarcia@outlook.com>
2014-04-02 15:10:00 -0300

    Created a specialization for visually impaired (low-vision) users.

--- source/SpaceshipGame/Application/Application.h
+++ source/SpaceshipGame/Application/Application.h
@@ -146,7 +146,22 @@ namespace sg
 
                 return pGameView;
             }
+            else if (profileName == "Visual Impairment: Low Vision")
+            {
+                uge::IGameViewSharedPointer pGameView(LIB_NEW sg::HumanView(m_Output.GetGraphics(),
+                                                                            m_Output.GetAudio(),
+                                                                            m_Resources.GetResourceCache(),
+                                                                            m_PlayerProfiles.GetCurrentProfile()));
 
+                vAddGameView(pGameView);
+
+#ifdef UGE_DEBUG_PHYSICS
+                std::shared_ptr<sg::HumanView> pCastGameView = std::dynamic_pointer_cast<sg::HumanView>(pGameView);
+                m_pGameLogic->vEnablePhysicsDebug(pCastGameView->GetPhysicsDebugRenderer());
+#endif
+
+                return pGameView;
+            }
 
             assert(0 && std::string("Invalid profile name: + " + profileName + "!").c_str());
 
