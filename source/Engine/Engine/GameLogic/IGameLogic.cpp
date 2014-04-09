e7f25a91b0a941de3e14c10c7fdae172daac281e
Franco Garcia <francoegarcia@outlook.com>
2014-04-09 10:38:33 -0300

    Added license to files.

--- source/Engine/Engine/GameLogic/IGameLogic.cpp
+++ source/Engine/Engine/GameLogic/IGameLogic.cpp
@@ -1,3 +1,19 @@
+/*
+  * (c) Copyright 2014 Franco Eusébio Garcia
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser GPL v3
+  * as published by the Free Software Foundation.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+  * http://www.gnu.org/licenses/lgpl-3.0.txt for more details.
+  *
+  * You should have received a copy of the GNU Lesser GPL v3
+  * along with this program; if not, write to the Free Software
+  */
+
 #include "GameEngineStd.h"
 
 #include "BaseGameLogic.h"
